c4b50ccf101607a78ee45a2b0d08c56379ce73f1
Andrew B <brei0017@gmail.com>
2019-01-02 16:57:25 -0800

    Update tutorial progress

--- Events-Delegates-and-Lambda-Expressions/BasicWindowsEvent/DelegatesAndEvents/Program.cs
+++ Events-Delegates-and-Lambda-Expressions/BasicWindowsEvent/DelegatesAndEvents/Program.cs
@@ -6,41 +6,57 @@ using System.Threading.Tasks;
 
 namespace DelegatesAndEvents
 {
-    public delegate void WorkPerformedHandler(int hours, WorkType workType);
+    //public delegate int WorkPerformedHandler(object sender, WorkPerformedEventArgs e);
+    // public delegate int WorkPerformedHandler(hours, WorkType);
 
 
     class Program
     {
         static void Main(string[] args)
         {
-            WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
-            WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+            //WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+            //WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+            //WorkPerformedHandler del3 = new WorkPerformedHandler(WorkPerformed3);
 
-            DoWork(del2);
+            //del1 += del2 + del3;
+
+            //int finalHours = del1(10, WorkType.GenerateReports);
+            //Console.WriteLine(finalHours);
+
+            var worker = new Worker();
+            
 
             Console.ReadLine();
         }
 
-        static void DoWork(WorkPerformedHandler del)
-        {
-            del(5, WorkType.Golf);
-        }
+        //        static void DoWork(WorkPerformedHandler del)
+        //        {
+        //            del(5, WorkType.Golf);
+        //        }
 
-        static void WorkPerformed1(int hours, WorkType workType)
-        {
-            Console.WriteLine("WorkPerformed1 called " + hours.ToString());
-        }
+        //        static int WorkPerformed1(int hours, WorkType workType)
+        //        {
+        //            Console.WriteLine("WorkPerformed1 called " + hours.ToString());
+        //            return hours + 1;
+        //        }
 
-        static void WorkPerformed2(int hours, WorkType workType)
-        {
-            Console.WriteLine("WorkPerformed2 called " + hours.ToString());
-        }
-    }
+        //        static int WorkPerformed2(int hours, WorkType workType)
+        //        {
+        //            Console.WriteLine("WorkPerformed2 called " + hours.ToString());
+        //            return hours + 2;
+        //        }
 
-    public enum WorkType
-    {
-        GoToMeetings,
-        Golf,
-        GenerateReports
+        //        static int WorkPerformed3(int hours, WorkType workType)
+        //        {
+        //            Console.WriteLine("WorkPerformed3 called " + hours.ToString());
+        //            return hours + 3;
+        //        }
+        //    }
+
+        //    public enum WorkType
+        //    {
+        //        GoToMeetings,
+        //        Golf,
+        //        GenerateReports
     }
 }
