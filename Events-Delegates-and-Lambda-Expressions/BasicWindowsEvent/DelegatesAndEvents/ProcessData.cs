558748977f405833ebf5ec4764cd7b817f5151b0
Andrew B <brei0017@gmail.com>
2019-01-03 12:52:40 -0800

    Save status before Action<T> unit

---
+++ Events-Delegates-and-Lambda-Expressions/BasicWindowsEvent/DelegatesAndEvents/ProcessData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DelegatesAndEvents.Program;
+
+namespace DelegatesAndEvents
+{
+    class ProcessData
+    {
+        public void Process(int x, int y, BizRulesDelegate del)
+        {
+            var result = del(x, y);
+            Console.WriteLine(result);
+        }
+    }
+}
