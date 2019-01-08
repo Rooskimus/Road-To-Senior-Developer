f7816df5cdafcf8727b0d235fb45bf0563e742e1
Andrew B <brei0017@gmail.com>
2019-01-08 12:48:40 -0800

    Upload Object Oriented Programming in CS tutorial files I made from Pluralsight course of that name.

---
+++ Object-Oriented-Programming-in-CS/ACM/ACM.BL/Customer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public static int InstanceCount { get; set; }
+
+        private string _lastName; //Backing field
+
+        public string LastName //Property delcaration
+        {
+            get
+            {
+                //Any code here
+                return _lastName;
+            }
+            set
+            {
+                //Any code here
+                _lastName = value;
+            }
+        }
+
+        public string FirstName { get; set; } // Auto-Implemented property; complier creates above for you.
+
+        public string EmailAddress { get; set; }
+
+        public int CustomerId { get; private set; } // A private setter restricts what can overwrite data.
+
+        public string FullName  // Because we derive this from our first and last name properties
+        {                       // No code should be able to directly set this, hence no setter.
+            get
+            {
+                string fullName = LastName;
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(fullName))
+                    {
+                        fullName += ", ";
+                    }
+                    fullName += FirstName;
+                }
+                return fullName;
+            }
+        }
+
+
+    }
+}
---
+++ Object-Oriented-Programming-in-CS/ACM/ACM.BLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACM.BLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACM.BLTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("28c93945-7e44-4210-8665-0173d3705a7e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
